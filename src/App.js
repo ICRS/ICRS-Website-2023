42166971f91786ea1f14314333a98748ee45215e
jak <jackx.crane@hotmail.com>
2024-01-22 20:57:59 +0000

    Fleshing out other pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Home from './Components/Home'
 import Layout from './Components/Layout'
 import Events from './Components/Events'
+import Projects from './Components/Projects'
 function App() {
   const routerPath = (process.env.NODE_ENV === 'production')
     ? 'ICRS-Website-2023/' : ''
@@ -14,6 +15,7 @@ function App() {
         <Route path={routerPath} element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="events" element={<Events />} />
+          <Route path="projects" element={<Projects />} />
 
         </Route>
       </Routes>
