994c19346d4a41ea8078e7a72dc44b1782dfb7af
jak <jackx.crane@hotmail.com>
2024-01-24 22:41:43 +0000

    trouble shooting

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Events from './Components/Events'
 import Projects from './Components/Projects'
 import Facilities from './Components/Facilities'
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'ICRS-Webiste-2023/'} element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="events" element={<Events />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="facilities" element={<Facilities />} />
-
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+return (<p>react app</p>)
 }
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path={'ICRS-Webiste-2023/'} element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="events" element={<Events />} />
+//           <Route path="projects" element={<Projects />} />
+//           <Route path="facilities" element={<Facilities />} />
+
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
